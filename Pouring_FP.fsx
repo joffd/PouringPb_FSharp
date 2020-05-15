3b9a5a355aed65a1a665fbb80c75868cfae510d5
joffd <jeff.fsharp@gmail.com>
2020-05-15 15:39:48 +0800

    Complete temp solution

--- Pouring_FP.fsx
+++ Pouring_FP.fsx
@@ -3,208 +3,220 @@
 open System
 open FSharpx.Collections
 
-module Helper =
-    let newList (index, value) (list: List<'T>) =
-        list
-        |> List.mapi (fun i v -> if i = index then value else v)
+module FP =
+    module Helper =
+        let newList (index, value) (list: List<'T>) =
+            list
+            |> List.mapi (fun i v -> if i = index then value else v)
 
-    let newArray (index, value) array =
-        array
-        |> Array.mapi (fun i v -> if i = index then value else v)    
+        let newArray (index, value) array =
+            array
+            |> Array.mapi (fun i v -> if i = index then value else v)    
 
-module Solver =
+    module Solver =
     
-    open Helper
+        open Helper
 
-    type Glass = int
+        type Glass = int
     
-    type Setup =
-    | CapacityAllEmpty of int array
-    | CapacityInitStatus of (int * int) array
+        type Setup =
+        | CapacityAllEmpty of int array
+        | CapacityInitStatus of (int * int) array
 
-    type Capacity = int array
+        type Capacity = int array
     
-    type Status =  int array
+        type Status =  int array
 
-    type Problem = {
-        Setup: Setup
-        Target: int
-    }
-
-    type Action =
-    | Fill of Glass
-    | Empty of Glass
-    | Pour of Glass * Glass
-
-    type InternalData = {
-        Pb: Problem
-        Capacity: int[]
-        InitStatus: int[]
-        Actions: List<Action>
-    }
-
-    type Path = {
-        History: List<Action>
-        EndStatus: Status
-    }
-
-    type Paths = Set<Path>
-
-
-    let nbGlasses pb = 
-        match (pb.Setup) with
-        | CapacityAllEmpty arr -> Array.length arr
-        | CapacityInitStatus arr -> Array.length arr
-
-    let capacity (pb: Problem) =
-        match pb.Setup with
-        | CapacityAllEmpty arr -> arr
-        | CapacityInitStatus arr -> (Array.unzip >> fst) arr
-
-    let initStatus (pb: Problem) =
-        match pb.Setup with
-        | CapacityAllEmpty arr -> arr |> Array.map (fun _ -> 0)
-        | CapacityInitStatus arr -> (Array.unzip >> snd) arr
-
-    let change (data: InternalData) (status: Status) (action: Action) : Status =
-        match action with
-        | Fill(g) -> newArray (g, data.Capacity.[g]) status
-        | Empty(g) -> newArray (g, 0) status
-        | Pour(g1, g2) ->
-            let amount = min (status.[g1]) (data.Capacity.[g2] - status.[g2])
-            status
-            |> newArray (g1, status.[g1] - amount)
-            |> newArray (g2, status.[g2] + amount)
-
-
-    let createActions (range: int[]) =   
-        seq {
-        for g in range do yield Action.Fill(g)               
-        for g in range do yield Action.Empty(g)
-        for g1 in range do
-            for g2 in range do
-                if g1 <> g2 then
-                    yield Action.Pour(g1, g2) }  
-
-
-
-    let pbToInternalPb (pb: Problem) =
-        let range =  [|0..(nbGlasses pb - 1)|]
-        {
-            Pb = pb
-            Capacity = capacity pb
-            InitStatus = initStatus pb
-            Actions = (createActions range) |> Seq.toList
+        type Problem = {
+            Setup: Setup
+            Target: int
         }
 
-    let extendPath (action: Action) (history: List<Action>) =
-        action :: history
+        type Action =
+        | Fill of Glass
+        | Empty of Glass
+        | Pour of Glass * Glass
 
-    let addAction (data: InternalData) (action: Action) (path: Path) =
-        {
-            History = action :: path.History
-            EndStatus = change data path.EndStatus action
+        type InternalData = {
+            Pb: Problem
+            Capacity: int[]
+            InitStatus: int[]
+            Actions: List<Action>
         }
 
-    let rec build (data: InternalData) (paths: Set<Path>) (explored : Set<Status>) : LazyList<Set<Path>> =
-        if (Set.isEmpty paths) then LazyList.empty<Set<Path>>
-        else
-            let more = 
-                seq {
-                    for path in paths do
-                        for action in data.Actions do
-                            let next = addAction data action path
-                            if not (explored |> Set.contains next.EndStatus) then
-                                yield next
-                } |> Set.ofSeq
-            LazyList.cons paths (build data more (Set.union (Set.map (fun x -> x.EndStatus) more) explored))
-
-
-    let solve (pb: Problem) =
-        let data = pbToInternalPb pb
-        let initPath = 
-            {
-                History = []
-                EndStatus = data.InitStatus
-            }
-        let pathsets =
-            build data (Set.ofList [initPath]) (Set.ofList [initPath.EndStatus])
-        seq {
-             for pathset in pathsets do
-                 for path in pathset do
-                     if (Array.contains pb.Target path.EndStatus) then
-                         yield path
+        type Path = {
+            History: List<Action>
+            EndStatus: Status
         }
 
-module Printer =
-    
-    open Solver
+        type Paths = Set<Path>
 
-    let printer (pb: Problem) (path: Path) =
-        let init = initStatus pb
-        let history = path.History |> List.rev
-        let internalPb = pbToInternalPb pb
-        printfn "Initial Status: %A" init
-        printfn "Final Status: %A" path.EndStatus
-        printfn "Number of actions: %i" (List.length history)
-        printfn "\n*** HISTORY ***"
 
-        let stringAction (action: Action) =
+        let nbGlasses pb = 
+            match (pb.Setup) with
+            | CapacityAllEmpty arr -> Array.length arr
+            | CapacityInitStatus arr -> Array.length arr
+
+        let capacity (pb: Problem) =
+            match pb.Setup with
+            | CapacityAllEmpty arr -> arr
+            | CapacityInitStatus arr -> (Array.unzip >> fst) arr
+
+        let initStatus (pb: Problem) =
+            match pb.Setup with
+            | CapacityAllEmpty arr -> arr |> Array.map (fun _ -> 0)
+            | CapacityInitStatus arr -> (Array.unzip >> snd) arr
+
+        let change (data: InternalData) (status: Status) (action: Action) : Status =
             match action with
-            | Fill g -> "Fill #" + (g.ToString())
-            | Empty g -> "Empty #" + (g.ToString())
-            | Pour (f, t) -> "Pour from #" + (f.ToString()) + " to #" + (t.ToString())
+            | Fill(g) -> newArray (g, data.Capacity.[g]) status
+            | Empty(g) -> newArray (g, 0) status
+            | Pour(g1, g2) ->
+                let amount = min (status.[g1]) (data.Capacity.[g2] - status.[g2])
+                status
+                |> newArray (g1, status.[g1] - amount)
+                |> newArray (g2, status.[g2] + amount)
 
 
-        let rec print (actions: List<Action>) (status: Status) =
-            match actions with
-            | [] -> printfn "DONE!\n\n"
-            | h :: t ->
-                let newStatus = change internalPb status h
-                printfn "%A  <=== %s" newStatus (stringAction h)
-                print t newStatus
+        let createActions (range: int[]) =   
+            seq {
+            for g in range do yield Action.Fill(g)               
+            for g in range do yield Action.Empty(g)
+            for g1 in range do
+                for g2 in range do
+                    if g1 <> g2 then
+                        yield Action.Pour(g1, g2) }  
 
-        print history init
 
-    let solvePrint (pb: Problem) =
-        solve pb
-        |> Seq.toList
-        |> List.groupBy (fun path -> List.length path.History)
-        |> function
-            | [] -> printfn "NO SOLUTION!"
-            | list ->
-                list
-                |> List.head
-                |> snd
-                |> List.iter (printer pb)
+
+        let pbToInternalPb (pb: Problem) =
+            let range =  [|0..(nbGlasses pb - 1)|]
+            {
+                Pb = pb
+                Capacity = capacity pb
+                InitStatus = initStatus pb
+                Actions = (createActions range) |> Seq.toList
+            }
+
+        let extendPath (action: Action) (history: List<Action>) =
+            action :: history
+
+        let addAction (data: InternalData) (action: Action) (path: Path) =
+            {
+                History = action :: path.History
+                EndStatus = change data path.EndStatus action
+            }
+
+        let rec build (data: InternalData) (paths: Set<Path>) (explored : Set<Status>) : LazyList<Set<Path>> =
+            if (Set.isEmpty paths) then LazyList.empty<Set<Path>>
+            else
+                let more = 
+                    seq {
+                        for path in paths do
+                            for action in data.Actions do
+                                let next = addAction data action path
+                                if not (explored |> Set.contains next.EndStatus) then
+                                    yield next
+                    } |> Set.ofSeq
+                LazyList.cons paths (build data more (Set.union (Set.map (fun x -> x.EndStatus) more) explored))
+
+
+        let solve (pb: Problem) =
+            let data = pbToInternalPb pb
+            let initPath = 
+                {
+                    History = []
+                    EndStatus = data.InitStatus
+                }
+            let pathSets =
+                build data (Set.ofList [initPath]) (Set.ofList [initPath.EndStatus])
+            pathSets
+            |> LazyList.iter (fun paths -> 
+                            paths
+                            |> Set.iter (fun path -> printfn "%A" path.EndStatus))
+            printfn "%i" (pathSets.Length())
+            seq {
+                 for pathSet in pathSets do
+                     for path in pathSet do
+                         if (Array.contains pb.Target path.EndStatus) then
+                             yield path
+            }
+
+    module Printer =
+    
+        open Solver
+
+        let printer (pb: Problem) (path: Path) =
+            let init = initStatus pb
+            let history = path.History |> List.rev
+            let internalPb = pbToInternalPb pb
+            printfn "Initial Status: %A" init
+            printfn "Final Status: %A" path.EndStatus
+            printfn "Number of actions: %i" (List.length history)
+            printfn "\n*** HISTORY ***"
+
+            let stringAction (action: Action) =
+                match action with
+                | Fill g -> "Fill #" + (g.ToString())
+                | Empty g -> "Empty #" + (g.ToString())
+                | Pour (f, t) -> "Pour from #" + (f.ToString()) + " to #" + (t.ToString())
+
+
+            let rec print (actions: List<Action>) (status: Status) =
+                match actions with
+                | [] -> printfn "DONE!\n\n"
+                | h :: t ->
+                    let newStatus = change internalPb status h
+                    printfn "%A  <=== %s" newStatus (stringAction h)
+                    print t newStatus
+
+            print history init
+
+        let solvePrint (pb: Problem) =
+            solve pb
+            |> Seq.toList
+            |> List.groupBy (fun path -> List.length path.History)
+            |> List.sortBy fst
+            |> function
+                | [] -> printfn "NO SOLUTION!"
+                | list ->
+                    list
+                    |> List.head
+                    |> snd
+                    |> List.iter (printer pb)
       
 // TEST
         
-open Solver
-open Printer
+
 
 let pb1 = {
-    Setup = CapacityInitStatus [|(7, 2) ; (9, 0) ; (5, 0)|]
-    Target = 8
+    FP.Solver.Setup = FP.Solver.CapacityInitStatus [|(7, 2) ; (9, 0) ; (5, 0)|]
+    FP.Solver.Target = 8
 }
 
-solvePrint pb1
+FP.Printer.solvePrint pb1
 
 let pb2 = {
-    Setup = CapacityAllEmpty [|3 ; 7|]
-    Target = 8
+    FP.Solver.Setup = FP.Solver.CapacityAllEmpty [|3 ; 7|]
+    FP.Solver.Target = 8
 }
 
-solvePrint pb2
+FP.Printer.solvePrint pb2
 
 
 let pb3 = {
-    Setup = CapacityAllEmpty [|3 ; 7|]
-    Target = 5
+    FP.Solver.Setup = FP.Solver.CapacityAllEmpty [|4;3 ; 7|]
+    FP.Solver.Target = 5
 }
 
-solvePrint pb3
+FP.Printer.solvePrint pb3
 
+let pb4 = {
+    FP.Solver.Setup = FP.Solver.CapacityAllEmpty [|1 ; 3|]
+    FP.Solver.Target = 2
+}
+
+FP.Printer.solvePrint pb4
 
 
 let ipb = Model.pbToInternalPb pb
--- Pouring_OO.fsx
+++ Pouring_OO.fsx
@@ -3,84 +3,254 @@
 open System
 open FSharpx.Collections
 
-module Helper =
-    let newList (index, value) (list: List<'T>) =
-        list
-        |> List.mapi (fun i v -> if i = index then value else v)
+module OO =
+    module Helper =
+        let newList (index, value) (list: List<'T>) =
+            list
+            |> List.mapi (fun i v -> if i = index then value else v)
 
-    let newArray (index, value) array =
-        array
-        |> Array.mapi (fun i v -> if i = index then value else v)    
+        let newArray (index, value) array =
+            array
+            |> Array.mapi (fun i v -> if i = index then value else v)    
 
-module Solver =
+    module Solver =
     
-    open Helper
+        open Helper
 
-    type Glass = int
+        type Glass = int
+
+        type Capacity = int[]
     
-    type Setup =
-    | CapacityAllEmpty of int array
-    | CapacityInitStatus of (int * int) array
+        type Setup =
+        | CapacityAllEmpty of int array
+        | CapacityInitStatus of (int * int) array
 
-    type Status = int[]
+        type Status = int[]
 
-    type Action =
-        abstract member Change: Status -> Status
+        [<AbstractClass>]
+        type Action() =
+            abstract member Change: Capacity -> Status -> Status
     
-    type Empty(glass: Glass) =
-        interface Action with
-            member __.Change(status: Status) =
-                newArray (glass, 0) status
+        type Empty(glass: Glass) =
+            inherit Action()
+                member __.Glass = glass
 
-    type Fill(glass: Glass, capacity: int) =
-        interface Action with
-            member __.Change(status: Status) =
-                newArray (glass, capacity) status
+                override __.Change _ (status: Status) =
+                    newArray (glass, 0) status
 
-    type Pour(fromGlass: Glass, toGlass: Glass, capacityTo: int) =
-        interface Action with
-            member __.Change(status: Status) =
-                let amount = min (status.[fromGlass]) (capacityTo - status.[toGlass])
-                status
-                |> newArray (toGlass, status.[toGlass] - amount)
-                |> newArray (toGlass, status.[toGlass] + amount)
+                override __.ToString() =
+                    "Empty #" + (glass.ToString())
 
-    type PouringPb(setup: Setup) =
-        // Capacity
-        let capacity = 
-            match setup with
-            | CapacityAllEmpty arr -> 
-                arr
-            | CapacityInitStatus arr ->
-                (Array.unzip >> fst) arr
+                override __.Equals (obj) =
+                    match obj with
+                    | :? Empty as empty ->
+                        (glass) = (empty.Glass)
+                    | _ -> false
+
+        type Fill(glass: Glass) =
+            inherit Action()
+                member __.Glass = glass
+
+                override __.Change (capacity: Capacity) (status: Status)  =
+                    newArray (glass, capacity.[glass]) status
+
+                override __.ToString() =
+                    "Fill #" + (glass.ToString())
+
+                override __.Equals (obj) =
+                    match obj with
+                    | :? Fill as fill ->
+                        glass = fill.Glass
+                    | _ -> false
+
+        type Pour(fromGlass: Glass, toGlass: Glass) =
+            inherit Action()
+                member __.FromGlass = fromGlass
+                member __.ToGlass = toGlass
+                                
+                override __.Change (capacity: Capacity) (status: Status) =
+                    let amount = min (status.[fromGlass]) (capacity.[toGlass] - status.[toGlass])
+                    status
+                    |> newArray (fromGlass, status.[fromGlass] - amount)
+                    |> newArray (toGlass, status.[toGlass] + amount)
+
+                override __.ToString() =
+                    "Pour from #" + (fromGlass.ToString()) + " to #" + (toGlass.ToString())
+
+                override __.Equals (obj) =
+                    match obj with
+                    | :? Pour as pour ->
+                        (fromGlass, toGlass) = (pour.FromGlass, pour.ToGlass)
+                    | _ -> false
+
+        type Path(history: List<Action>, capacity: Capacity, endStatus: Status) =
+            
+            member __.EndStatus = endStatus  
+
+            member __.History = history
+
+            member __.Capacity = capacity
+
+            member __.Extend(action: Action) =
+                let newEndStatus = action.Change capacity endStatus
+                new Path(action :: history, capacity, newEndStatus)
+
+            override __ .GetHashCode() =
+                hash (history, capacity, endStatus)
+
+            override this.Equals (obj) =
+                match obj with
+                | :? Path as path ->
+                    (history, capacity, endStatus) = (path.History, path.Capacity, path.EndStatus)
+                | _ -> false
+
+            interface IComparable<Path> with
+                member this.CompareTo (other: Path) =
+                    other.GetHashCode() - this.GetHashCode()
+
+            interface IComparable with
+                override this.CompareTo other =
+                    (this :> IComparable<Path>).CompareTo (other :?> Path)
+
+            member __.Print(initStatus: Status) =
+                let init = initStatus
+                let history = history |> List.rev
+                printfn "Initial Status: %A" init
+                printfn "Final Status: %A" endStatus
+                printfn "Number of actions: %i" (List.length history)
+                printfn "\n*** HISTORY ***"
+
+                let stringAction (action: Action) =
+                    match action with
+                    | :? Fill as fill -> (fill.ToString())
+                    | :? Empty as empty -> (empty.ToString())
+                    | :? Pour as pour -> (pour.ToString())
+                    | _ -> raise <| System.SystemException()
+
+
+                let rec print (actions: List<Action>) (status: Status) =
+                    match actions with
+                    | [] -> printfn "DONE!\n\n"
+                    | h :: t ->
+                        let newStatus = h.Change capacity status
+                        printfn "%A  <=== %s" newStatus (stringAction h)
+                        print t newStatus
+
+                print history init
+            
+            
+
+        type PouringPb(setup: Setup) =
+            
+            // Capacity
+            let capacity = 
+                match setup with
+                | CapacityAllEmpty arr -> 
+                    arr
+                | CapacityInitStatus arr ->
+                    (Array.unzip >> fst) arr
         
-        // Initial Status
-        let initStatus =
-            match setup with
-            | CapacityAllEmpty arr -> 
-                arr
-                |> Array.map (fun _ -> 0)
-            | CapacityInitStatus arr ->
-                (Array.unzip >> snd) arr
+            // Initial Status
+            let initStatus =
+                match setup with
+                | CapacityAllEmpty arr -> 
+                    arr
+                    |> Array.map (fun _ -> 0)
+                | CapacityInitStatus arr ->
+                    (Array.unzip >> snd) arr
                 
-
-        let range = [|0 .. (Array.length capacity - 1) |]
+            // Define the range of glasses
+            let range = [|0 .. (Array.length capacity - 1) |]
         
-        let actions : seq<Action> =
-            seq {
-                for g in range do yield Fill(g, capacity.[g])               
-                for g in range do yield Empty(g)
-                for g1 in range do
-                    for g2 in range do
-                        if g1 <> g2 then
-                            yield Pour(g1, g2, capacity.[g2]) }  
+            // List of potential actions
+            let actions : seq<Action> =
+                seq {
+                    for g in range do yield Fill(g)               
+                    for g in range do yield Empty(g)
+                    for g1 in range do
+                        for g2 in range do
+                            if g1 <> g2 then
+                                yield Pour(g1, g2) }
+
+            // Build a LazyList of Set of Paths. The point is to create paths that are unique
+            // to avoid circular paths
+            let rec build (paths: Set<Path>) (explored : Set<Status>) : LazyList<Set<Path>> =
+                if (Set.isEmpty paths) then LazyList.empty<Set<Path>>
+                else
+                    let more = 
+                        seq {
+                            for path in paths do
+                                for action in actions do
+                                    let next = path.Extend action
+                                    if not (Set.contains (next.EndStatus) explored) then
+                                        yield next
+                        } |> Set.ofSeq
+                    let newExplored = Set.map (fun (x: Path) -> x.EndStatus) more
+                    LazyList.cons paths (build more (newExplored + explored))
+
+            let initPath = new Path([], capacity, initStatus)
+
+            let solve (target: int) =
+                let pathSets =
+                    build (Set.ofList [initPath]) (Set.ofList [initPath.EndStatus])
+
+                seq {
+                    for pathSet in pathSets do
+                        for path in pathSet do
+                            if (Array.contains target path.EndStatus) then
+                                yield path
+                }
 
 
-        member __.Capacity = capacity
-        member __.InitStatus = initStatus
+            let printSolve(paths: seq<Path>) =
+                paths
+                |> Seq.toList
+                |> List.groupBy (fun path -> List.length path.History)
+                |> List.sortBy fst
+                |> function
+                    | [] -> printfn "NO SOLUTION!"
+                    | list ->
+                        list
+                        |> List.head
+                        |> snd
+                        |> List.iter (fun p -> (p.Print(initStatus)))
 
-open Solver
+            member __.Solve(target: int) = 
+                solve target
 
-let pb1 = CapacityAllEmpty [|3 ; 7|]
-let pouring1 = PouringPb(pb1)
+            member __.SolveAndPrint(target: int) =
+                target
+                |> solve
+                |> printSolve
+
+            member __.Actions = actions
+
+
+
+let pb1 = OO.Solver.CapacityAllEmpty [|4;3 ; 7|]
+let pouring1 = OO.Solver.PouringPb(pb1)
+pouring1.SolveAndPrint(5)
+pouring1.Solve 5
+|>Seq.toList
+
+pouring1.Actions
+|> Seq.toList
+
+let actions1 : List<OO.Solver.Action>= [OO.Solver.Pour(1,0)]
+let actions2 : List<OO.Solver.Action>= [OO.Solver.Pour(1,0)]
+let status1 = [|0;0|]
+let status2 = [|0;1|]
+let status3 = [|0;0|]
+let capacity1 = [|3 ; 7|]
+let capacity2 = [|3 ; 7|]
+
+let path1 = new OO.Solver.Path(actions1, capacity1, status1)
+let path2 = new OO.Solver.Path(actions2, capacity2, status2)
+
+let set1 = Set.singleton path1
+let set2 = set1.Add path2
+set2.Count
+actions1 = actions2
+path1.Equals(path2)
+
 
